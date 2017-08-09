02261244644446b3f72cbabe6f72fb7820309e89
Ricardo Martin Marcucci <mmarcucci@ucc.edu.ar>
2017-08-09 11:59:03 -0300

    Implementadas las clases color y cuadrado

--- 01_POO/Color.cpp
+++ 01_POO/Color.cpp
@@ -1,1 +1,18 @@
 #include "Color.h"
+
+
+uint32_t Color::getColor() {
+    uint32_t tmp = 0;
+
+    tmp = rojo * 256 * 256;
+    tmp += verde * 256;
+    tmp += azul;
+    return tmp;
+}
+
+void Color::tenirColor(uint8_t rojo, uint8_t verde, uint8_t azul) {
+    this->rojo += rojo;
+    this->verde += verde;
+    this->azul += azul;
+
+}
--- 01_POO/Color.h
+++ 01_POO/Color.h
@@ -1,9 +1,27 @@
+#include <stdint.h>
+
 #ifndef REPASO_COLOR_H
 #define REPASO_COLOR_H
 
+/*
+    **Propiedades:**
+        * Rojo
+        * Verde
+        * Azul
 
+    **Métodos:**
+        * getColor
+        * tenirColor
+ */
 class Color {
+private:
+    uint8_t rojo;
+    uint8_t verde;
+    uint8_t azul;
+public:
+    uint32_t getColor();
 
+    void tenirColor(uint8_t rojo, uint8_t verde, uint8_t azul);
 };
 
 
