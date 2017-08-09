a24e76621e65e04bd642ad8ab1302dfa31b07be3
Ricardo Martin Marcucci <mmarcucci@ucc.edu.ar>
2017-08-09 10:39:57 -0300

    Se implemento el ejercico III

--- 03_Excepciones/Calculadora.h
+++ 03_Excepciones/Calculadora.h
@@ -2,8 +2,46 @@
 #define REPASO_CALCULADORA_H
 
 
-class Calculadora {
+template<class T>
 
+class Calculadora {
+private:
+    T A;
+    T B;
+public:
+    T getB() const {
+        return B;
+    }
+
+    void setB(T B) {
+        Calculadora::B = B;
+    }
+
+    T getA() const {
+        return A;
+    }
+
+    void setA(T A) {
+        Calculadora::A = A;
+    }
+
+    T sumar() {
+        return A + B;
+    }
+
+    T restar() {
+        return A - B;
+    }
+
+    T multiplicar() {
+        return A * B;
+    }
+
+    T dividir() {
+        if(B == 0)
+            throw 3;
+        return A / B;
+    }
 };
 
 
--- 03_Excepciones/main.cpp
+++ 03_Excepciones/main.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 int main() {
-    cout << "Ejercicio 00/03\n" << endl;
+    Calculadora<int > calc;
+
+    calc.setA(300);
+    calc.setB(3);
+
+    try {
+        cout << calc.dividir() << endl;
+    }catch (int e) {
+        cout << "B no puede ser 0: " << e << endl;
+    }
+
     return 0;
 }
